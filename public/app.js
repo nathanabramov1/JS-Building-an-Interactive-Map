1e249832755887697aaf9c0d030e4809a62550c1
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-06-04 12:32:31 -0400

    not finished

--- public/app.js
+++ public/app.js
@@ -3,19 +3,30 @@
 
 
 // Create map
-
+const myMap = L.map('map', {
+    center:[48.868672, 2.342130],
+    zoom: 12,
+})
 
 
 
 // Add openstreetmap tiles
-
+// Add OpenStreetMap tiles:
+L.tileLayer('https://.tile.openstreetmap.org///.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
 
 // Create and add a geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
+// i am up to here//
 
 
 
